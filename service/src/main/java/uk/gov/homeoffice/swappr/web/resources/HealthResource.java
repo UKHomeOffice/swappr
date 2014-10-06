5c70bb53afd8d8e0c931922c1b2da583480c7276
Kevin Potgieter <kevin.potgieter@bluedawnsolutions.co.uk>
2014-10-06 13:43:45 +0100

    - Change HelloWorldResource to HealthResource - Tidy up imports on JerseyResourceConfig.java

--- service/src/main/java/uk/gov/homeoffice/swappr/web/resources/HealthResource.java
+++ service/src/main/java/uk/gov/homeoffice/swappr/web/resources/HealthResource.java
@@ -4,12 +4,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-@Path("helloworld")
-public class HelloWorldResource {
+@Path("ping")
+public class HealthResource {
 
     @GET
     @Produces("text/plain")
     public String getHello() {
-        return "Hello World";
+        return "pong";
     }
 }
