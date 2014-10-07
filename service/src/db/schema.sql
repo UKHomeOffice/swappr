eb4ececf1c6872dfb90d992dc3a5f717db92088e
pyruby <chris@pyruby.co.uk>
2014-10-07 14:21:23 +0100

    - Add database creation and seed scripts.

---
+++ service/src/db/schema.sql
@@ -0,0 +1,12 @@
+create database swappr;
+grant usage on *.* to swappr@localhost identified by 'swappr';
+grant all privileges on swappr.* to swappr@localhost;
+use swappr;
+
+create table users (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+name varchar(250),
+password varchar(250),
+role varchar(50),
+PRIMARY KEY(id)
+)engine=InnoDB;
---
+++ service/src/db/seed.sql
@@ -0,0 +1,4 @@
+
+insert into swappr.users values (1, 'admin', 'ictIhHE+0928NYkwQOgJ7C+FpD4=', 'admin');
+
+commit;
