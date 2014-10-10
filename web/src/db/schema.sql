6feeed099ff502a0cf3dd2b7bc3ce0dc083912c5
pyruby <chris@pyruby.co.uk>
2014-10-10 17:38:56 +0100

    - Fix password truncation error

--- web/src/db/schema.sql
+++ web/src/db/schema.sql
@@ -6,7 +6,7 @@ use swappr;
 
 create table users (
     username varchar(50) not null primary key,
-    password varchar(50) not null,
+    password varchar(250) not null,
     enabled boolean not null
 ) engine = InnoDb;
 
