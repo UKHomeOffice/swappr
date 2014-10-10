bb53fee4a1c5a7716563a23d9b5e2ca7f41a08be
Martin Gladdish <martin@martingladdish.co.uk>
2014-10-10 11:05:21 +0100

    Tidied imports

--- web/src/main/java/uk/gov/homeofficedigital/swappr/Application.java
+++ web/src/main/java/uk/gov/homeofficedigital/swappr/Application.java
@@ -5,9 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import uk.gov.homeofficedigital.swappr.controllers.Controllers;
 import uk.gov.homeofficedigital.swappr.daos.DAOs;
 
