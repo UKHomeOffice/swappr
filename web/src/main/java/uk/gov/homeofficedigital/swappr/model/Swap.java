e291914deb9fe8a585422dda8680c058de5eb7e5
Martin Gladdish <martin@martingladdish.co.uk>
2014-10-16 10:42:14 +0100

    Added missing hashCode and equals methods

--- web/src/main/java/uk/gov/homeofficedigital/swappr/model/Swap.java
+++ web/src/main/java/uk/gov/homeofficedigital/swappr/model/Swap.java
@@ -1,5 +1,10 @@
 package uk.gov.homeofficedigital.swappr.model;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
 import java.time.LocalDate;
 
 public class Swap {
@@ -42,5 +47,20 @@ public class Swap {
 
     public SwapStatus getStatus() {
         return status;
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
     }
 }
