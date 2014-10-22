dc7229d1fb48b7637b98b7b54bd03303faa3c1c5
Martin Gladdish <martin@martingladdish.co.uk>
2014-10-22 14:30:41 +0100

    Home page is now driven by day, regardless of shift

---
+++ web/src/main/java/uk/gov/homeofficedigital/swappr/model/DayView.java
@@ -0,0 +1,23 @@
+package uk.gov.homeofficedigital.swappr.model;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public class DayView {
+
+    private final LocalDate day;
+    private final Optional<RotaView> rotaView;
+
+    public DayView(LocalDate day, Optional<RotaView> rotaView) {
+        this.day = day;
+        this.rotaView = rotaView;
+    }
+
+    public LocalDate getDay() {
+        return day;
+    }
+
+    public Optional<RotaView> getRotaView() {
+        return rotaView;
+    }
+}
