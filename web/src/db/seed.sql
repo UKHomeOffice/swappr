50b04233225257c916ccf290b9db552f84b7cdcd
pyruby <chris@pyruby.co.uk>
2014-10-23 11:25:32 +0100

    - Add seed.sql to pre-populate shift types

---
+++ web/src/db/seed.sql
@@ -0,0 +1,8 @@
+use swappr;
+
+insert into shiftType (code, description) values ('B1H', 'Early');
+insert into shiftType (code, description) values ('BFH', 'Early');
+insert into shiftType (code, description) values ('C1H', 'Mid');
+insert into shiftType (code, description) values ('CFH', 'Mid');
+insert into shiftType (code, description) values ('S1H', 'Late');
+insert into shiftType (code, description) values ('T1H', 'Late');
