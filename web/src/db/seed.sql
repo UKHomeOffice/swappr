68b585712d90e6a9d7dd7fcf2d24f3db15ed395e
pyruby <chris@pyruby.co.uk>
2014-10-23 11:40:44 +0100

    - Admin user now exists in the DB so can 'Do stuff'

--- web/src/db/seed.sql
+++ web/src/db/seed.sql
@@ -1,5 +1,9 @@
 use swappr;
 
+
+insert into users (username, password, enabled) values ('admin', 'f8f0cf7ba6486ae73858572eeff1830e67978d0426e7b894ada42e8d0af38a7658a1d1a8ce81ff2e', true);
+insert into authorities (username, authority) values ('admin', 'ROLE_ADMIN');
+
 insert into shiftType (code, description) values ('B1H', 'Early');
 insert into shiftType (code, description) values ('BFH', 'Early');
 insert into shiftType (code, description) values ('C1H', 'Mid');
--- web/src/test/resources/data.sql
+++ web/src/test/resources/data.sql
@@ -1,4 +1,6 @@
 
+insert into users (username, password, enabled) values ('admin', 'f8f0cf7ba6486ae73858572eeff1830e67978d0426e7b894ada42e8d0af38a7658a1d1a8ce81ff2e', true);
+insert into authorities (username, authority) values ('admin', 'ROLE_ADMIN');
 insert into users (username, password, enabled) values ('Bill', 'password', true);
 insert into authorities (username, authority) values ('Bill', 'ROLE_USER');
 insert into users (username, password, enabled) values ('Ben', 'password', true);
