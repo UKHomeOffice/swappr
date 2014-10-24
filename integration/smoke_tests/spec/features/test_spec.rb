563fe8c99d9037e688ee6670bb9f0d419f45aa74
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2014-10-24 22:02:54 +0100

    Use 'feature' syntax

--- integration/smoke_tests/spec/features/test_spec.rb
+++ integration/smoke_tests/spec/features/test_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 require 'step_definitions/common_steps'
 
-describe "the signin process", :type => :feature do
-  before :each do
-    login_user
+feature "Logging in" do
+
+  background do
+    login_user_bill
   end
 
-  it "signs me in" do
+  scenario "Signing in takes you to the 'You' timeline" do
     expect(page).to have_selector('.sprite-user')
   end
+
 end
