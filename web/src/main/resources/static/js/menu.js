38b62a870966a257e669396b70e4f8e8be4cee29
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2014-10-27 16:25:22 +0000

    Remove jQuery dependency

--- web/src/main/resources/static/js/menu.js
+++ web/src/main/resources/static/js/menu.js
@@ -1,7 +1,23 @@
-(function($){
-   'use strict';
+(function () {
+    'use strict';
 
-   $('.js-menu').on('click', function() {
-       $(this).toggleClass('is-active');
-   });
-}(jQuery));
+    var el = document.querySelector('.js-menu');
+
+    function toggleClass() {
+        var pattern = /(?:^|\s)is-active(?!\S)/g,
+            isActive = regex.test(el.className);
+
+        if (isActive) {
+            el.className = el.className.replace(pattern, '');
+        } else {
+            el.className += ' is-active';
+        }
+    }
+
+    if (el.addEventListener) {
+        el.addEventListener('click', toggleClass, false);
+    } else if (el.attachEvent) {
+        el.attachEvent('onclick', toggleClass);
+    }
+
+}());
