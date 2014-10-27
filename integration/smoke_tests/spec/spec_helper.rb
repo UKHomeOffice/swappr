9deaeb0ac391e961c98f444e87ca617e1040b5dd
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2014-10-27 16:56:46 +0000

    Ignore JS errors when running smoke tests. C

--- integration/smoke_tests/spec/spec_helper.rb
+++ integration/smoke_tests/spec/spec_helper.rb
@@ -2,6 +2,12 @@ require 'capybara/rspec'
 require 'capybara/poltergeist'
 require 'pry'
 
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {
+    :js_errors => false
+  })
+end
+
 Capybara.default_driver = :poltergeist
 
 HOST = ENV['HOST'] || 'http://localhost:8080'
