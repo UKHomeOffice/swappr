92843901eac2714c0be4f6ff96ae146983acf478
Martin Gladdish <martin@martingladdish.co.uk>
2014-10-28 14:00:59 +0000

    Starting to put test support around RotaService

--- web/src/test/java/uk/gov/homeofficedigital/swappr/service/RotaServiceTest.java
+++ web/src/test/java/uk/gov/homeofficedigital/swappr/service/RotaServiceTest.java
@@ -8,14 +8,15 @@ import uk.gov.homeofficedigital.swappr.daos.VolunteerDao;
 import uk.gov.homeofficedigital.swappr.model.*;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import static com.natpryce.makeiteasy.MakeItEasy.a;
 import static com.natpryce.makeiteasy.MakeItEasy.make;
 
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class RotaServiceTest {
 
@@ -45,5 +46,36 @@ public class RotaServiceTest {
         service.volunteerSwap(rota, offer);
 
         verify(volunteerDao).create(eq(rota), eq(offer), eq(VolunteerStatus.CREATED));
+    }
+
+    @Test
+    public void acceptVolunteerShouldUpdateTheVolunteerStatusToAccepted() {
+        Volunteer volunteer = make(a(VolunteerMaker.Volunteer));
+        service.acceptVolunteer(volunteer);
+
+        verify(volunteerDao).updateStatus(eq(volunteer), eq(VolunteerStatus.ACCEPTED));
+    }
+
+    @Test
+    public void acceptVolunteerShouldUpdateTheOfferStatusToAccepted() {
+        Volunteer volunteer = make(a(VolunteerMaker.Volunteer));
+        service.acceptVolunteer(volunteer);
+
+        verify(offerDao).updateStatus(eq(volunteer.getSwapTo()), eq(OfferStatus.ACCEPTED));
+    }
+
+    @Test
+    public void acceptVolunteerShouldUpdateAllOtherVolunteersStatusesToRejected() {
+        Volunteer volunteer = make(a(VolunteerMaker.Volunteer));
+        Volunteer volunteer2 = make(a(VolunteerMaker.Volunteer));
+        Volunteer volunteer3 = make(a(VolunteerMaker.Volunteer));
+
+
+        when(volunteerDao.findByOffer(volunteer.getSwapTo())).thenReturn(new HashSet<Volunteer>(Arrays.asList(volunteer, volunteer2, volunteer3)));
+
+        service.acceptVolunteer(volunteer);
+
+        verify(volunteerDao).updateStatus(eq(volunteer2), eq(VolunteerStatus.REJECTED));
+        verify(volunteerDao).updateStatus(eq(volunteer3), eq(VolunteerStatus.REJECTED));
     }
 }
