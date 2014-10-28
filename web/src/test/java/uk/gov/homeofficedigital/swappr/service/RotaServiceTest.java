8cf66ba5488ea33393c4f096c4c9ea5b48b78dda
Martin Gladdish <martin@martingladdish.co.uk>
2014-10-28 15:46:56 +0000

    Added test coverage for findMyRotas and findAllRotas

--- web/src/test/java/uk/gov/homeofficedigital/swappr/service/RotaServiceTest.java
+++ web/src/test/java/uk/gov/homeofficedigital/swappr/service/RotaServiceTest.java
@@ -2,6 +2,8 @@ package uk.gov.homeofficedigital.swappr.service;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.security.core.userdetails.User;
+import uk.gov.homeofficedigital.swappr.controllers.views.RotaView;
 import uk.gov.homeofficedigital.swappr.daos.OfferDao;
 import uk.gov.homeofficedigital.swappr.daos.RotaDao;
 import uk.gov.homeofficedigital.swappr.daos.VolunteerDao;
@@ -10,9 +12,13 @@ import uk.gov.homeofficedigital.swappr.model.*;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 import static com.natpryce.makeiteasy.MakeItEasy.a;
 import static com.natpryce.makeiteasy.MakeItEasy.make;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.*;
 
 public class RotaServiceTest {
@@ -68,7 +74,7 @@ public class RotaServiceTest {
         Volunteer volunteer3 = make(a(VolunteerMaker.Volunteer));
 
 
-        when(volunteerDao.findByOffer(volunteer.getSwapTo())).thenReturn(new HashSet<Volunteer>(Arrays.asList(volunteer, volunteer2, volunteer3)));
+        when(volunteerDao.findByOffer(volunteer.getSwapTo())).thenReturn(new HashSet<>(Arrays.asList(volunteer, volunteer2, volunteer3)));
 
         service.acceptVolunteer(volunteer);
 
@@ -117,6 +123,43 @@ public class RotaServiceTest {
 
         verify(offerDao).updateStatus(volunteer.getSwapTo(), OfferStatus.DENIED);
     }
+
+    @Test
+    public void findMyRotasShouldCollateTheRotasOffersAndVolunteers() {
+        User user = make(a(UserMaker.User));
+        Rota rota1 = make(a(RotaMaker.Rota));
+        Rota rota2 = make(a(RotaMaker.Rota));
+        when(rotaDao.findByWorker(user)).thenReturn(new HashSet<>(Arrays.asList(rota1, rota2)));
+        Set<Offer> rota1Offers = new HashSet<>(Arrays.asList(make(a(OfferMaker.Offer))));
+        Set<Volunteer> rota1Volunteers = new HashSet<>(Arrays.asList(make(a(VolunteerMaker.Volunteer))));
+        when(offerDao.findByRota(rota1)).thenReturn(new HashSet<>(rota1Offers));
+        when(volunteerDao.findByRota(rota1)).thenReturn(new HashSet<>(rota1Volunteers));
+
+        Set<RotaView> myRotas = service.findMyRotas(user);
+
+        assertThat(myRotas, hasSize(2));
+
+        assertThat(myRotas, hasItem(new RotaView(rota1, rota1Offers, rota1Volunteers)));
+        assertThat(myRotas, hasItem(new RotaView(rota2, new HashSet<>(), new HashSet<>())));
+    }
+
+    @Test
+    public void findAllRotasShouldCollateTheRotasOffersAndVolunteers() {
+        Rota rota1 = make(a(RotaMaker.Rota));
+        Rota rota2 = make(a(RotaMaker.Rota));
+        when(rotaDao.findAll()).thenReturn(new HashSet<>(Arrays.asList(rota1, rota2)));
+        Set<Offer> rota1Offers = new HashSet<>(Arrays.asList(make(a(OfferMaker.Offer))));
+        Set<Volunteer> rota1Volunteers = new HashSet<>(Arrays.asList(make(a(VolunteerMaker.Volunteer))));
+        when(offerDao.findByRota(rota1)).thenReturn(new HashSet<>(rota1Offers));
+        when(volunteerDao.findByRota(rota1)).thenReturn(new HashSet<>(rota1Volunteers));
+
+        Set<RotaView> allRotas = service.findAllRotas();
+
+        assertThat(allRotas, hasSize(2));
+        assertThat(allRotas, hasItem(new RotaView(rota1, rota1Offers, rota1Volunteers)));
+        assertThat(allRotas, hasItem(new RotaView(rota2, new HashSet<>(), new HashSet<>())));
+    }
+
 
 
 }
