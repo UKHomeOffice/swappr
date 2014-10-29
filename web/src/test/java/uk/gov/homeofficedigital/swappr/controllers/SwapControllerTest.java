41f74331b01429b73970b59f461ff5a7c59116bc
Martin Gladdish <martin@martingladdish.co.uk>
2014-10-29 12:14:14 +0000

    Firming up test coverage on SwapController

--- web/src/test/java/uk/gov/homeofficedigital/swappr/controllers/SwapControllerTest.java
+++ web/src/test/java/uk/gov/homeofficedigital/swappr/controllers/SwapControllerTest.java
@@ -1,10 +1,16 @@
 package uk.gov.homeofficedigital.swappr.controllers;
 
 import org.junit.Test;
+import org.springframework.ui.Model;
+import org.springframework.validation.support.BindingAwareModelMap;
 import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
+import uk.gov.homeofficedigital.swappr.controllers.exceptions.RotaNotFoundException;
+import uk.gov.homeofficedigital.swappr.controllers.forms.SwapForm;
 import uk.gov.homeofficedigital.swappr.daos.OfferDao;
 import uk.gov.homeofficedigital.swappr.daos.RotaDao;
 import uk.gov.homeofficedigital.swappr.daos.VolunteerDao;
+import uk.gov.homeofficedigital.swappr.model.Rota;
+import uk.gov.homeofficedigital.swappr.model.RotaMaker;
 import uk.gov.homeofficedigital.swappr.model.Volunteer;
 import uk.gov.homeofficedigital.swappr.model.VolunteerMaker;
 import uk.gov.homeofficedigital.swappr.service.RotaService;
@@ -24,6 +30,29 @@ public class SwapControllerTest {
     private VolunteerDao volunteerDao = mock(VolunteerDao.class);
     private ControllerHelper helper = mock(ControllerHelper.class);
     private SwapController swapController = new SwapController(rotaDao, rotaService, offerDao, volunteerDao, helper);
+
+    @Test(expected = RotaNotFoundException.class)
+    public void view_shouldThrowAnExceptionIfTheRotaCannotBeFound() {
+
+        when(rotaDao.findById(21l)).thenReturn(Optional.empty());
+
+        swapController.view(21l, new BindingAwareModelMap());
+    }
+
+    @Test
+    public void view_shouldPopulateModelWithTheGivenRotasShift() {
+
+        Rota rota = make(a(RotaMaker.Rota));
+        when(rotaDao.findById(22l)).thenReturn(Optional.of(rota));
+
+        Model model = new BindingAwareModelMap();
+        swapController.view(22l, model);
+
+        SwapForm form = (SwapForm) model.asMap().get("swap");
+        assertEquals(form.getFromDate(), rota.getShift().getDate());
+        assertEquals(form.getFromShiftType(), rota.getShift().getType());
+        assertEquals(form.getToDate(), rota.getShift().getDate());
+    }
 
     @Test
     public void acceptSwap_shouldFindTheVolunteerAndDelegateToTheRotaService() throws Exception {
